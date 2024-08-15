705df2ed2d46cc3fb58d455b73e0997d9cc82cee
Robokat9 <kolesoakaterinapo433@gmail.com>
2024-08-15 16:53:06 +0500

    Добавьте файлы проекта.

---
+++ OpenStatistic/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace OpenStatistic
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle5 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle6 = new DataGridViewCellStyle();
+            dataGridView1 = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            Column4 = new DataGridViewTextBoxColumn();
+            Column5 = new DataGridViewTextBoxColumn();
+            Column6 = new DataGridViewTextBoxColumn();
+            Column7 = new DataGridViewTextBoxColumn();
+            Column8 = new DataGridViewTextBoxColumn();
+            Column9 = new DataGridViewTextBoxColumn();
+            openFileDialog1 = new OpenFileDialog();
+            textBox1 = new TextBox();
+            groupBox1 = new GroupBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle4.Font = new Font("Microsoft Sans Serif", 8.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+            dataGridView1.BackgroundColor = SystemColors.ControlDarkDark;
+            dataGridView1.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle5.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle5.BackColor = Color.FromArgb(20, 150, 91);
+            dataGridViewCellStyle5.Font = new Font("Segoe UI", 8F);
+            dataGridViewCellStyle5.ForeColor = SystemColors.ControlLightLight;
+            dataGridViewCellStyle5.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = DataGridViewTriState.True;
+            dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            dataGridView1.ColumnHeadersHeight = 55;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4, Column5, Column6, Column7, Column8, Column9 });
+            dataGridViewCellStyle6.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = Color.White;
+            dataGridViewCellStyle6.Font = new Font("Montserrat", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle6.ForeColor = SystemColors.ActiveCaptionText;
+            dataGridViewCellStyle6.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = DataGridViewTriState.True;
+            dataGridView1.DefaultCellStyle = dataGridViewCellStyle6;
+            dataGridView1.Dock = DockStyle.Bottom;
+            dataGridView1.EnableHeadersVisualStyles = false;
+            dataGridView1.GridColor = Color.FromArgb(0, 0, 192);
+            dataGridView1.Location = new Point(0, 83);
+            dataGridView1.Margin = new Padding(3, 4, 3, 4);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.DefaultCellStyle.Font = new Font("Microsoft Sans Serif", 8.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dataGridView1.Size = new Size(1487, 517);
+            dataGridView1.TabIndex = 0;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // Column1
+            // 
+            Column1.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column1.HeaderText = "Номер программы";
+            Column1.MinimumWidth = 6;
+            Column1.Name = "Column1";
+            Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "Дата";
+            Column2.MinimumWidth = 6;
+            Column2.Name = "Column2";
+            Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            Column3.HeaderText = "Программа";
+            Column3.MinimumWidth = 6;
+            Column3.Name = "Column3";
+            Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            Column4.HeaderText = "Время цикла";
+            Column4.MinimumWidth = 6;
+            Column4.Name = "Column4";
+            Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            Column5.HeaderText = "Время устранения остановки";
+            Column5.MinimumWidth = 6;
+            Column5.Name = "Column5";
+            Column5.ReadOnly = true;
+            // 
+            // Column6
+            // 
+            Column6.HeaderText = "Время работы робота 1";
+            Column6.MinimumWidth = 6;
+            Column6.Name = "Column6";
+            Column6.ReadOnly = true;
+            // 
+            // Column7
+            // 
+            Column7.HeaderText = "Время работы робота 2";
+            Column7.MinimumWidth = 6;
+            Column7.Name = "Column7";
+            Column7.ReadOnly = true;
+            // 
+            // Column8
+            // 
+            Column8.HeaderText = "Количество";
+            Column8.MinimumWidth = 6;
+            Column8.Name = "Column8";
+            Column8.ReadOnly = true;
+            // 
+            // Column9
+            // 
+            Column9.HeaderText = "Логин";
+            Column9.MinimumWidth = 6;
+            Column9.Name = "Column9";
+            Column9.ReadOnly = true;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(11, 27);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(1125, 27);
+            textBox1.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Location = new Point(0, 11);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(1143, 67);
+            groupBox1.TabIndex = 5;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Название файла";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Green;
+            button1.Cursor = Cursors.Hand;
+            button1.FlatAppearance.BorderColor = Color.Green;
+            button1.FlatAppearance.MouseDownBackColor = Color.LimeGreen;
+            button1.FlatAppearance.MouseOverBackColor = Color.Green;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 9F);
+            button1.ForeColor = SystemColors.ControlLightLight;
+            button1.Location = new Point(1149, 23);
+            button1.Name = "button1";
+            button1.Size = new Size(338, 53);
+            button1.TabIndex = 6;
+            button1.Text = "Импортировать";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            button1.MouseDown += button1_MouseDown;
+            button1.MouseUp += button1_MouseUp;
+            // 
+            // Form1
+            // 
+            AcceptButton = button1;
+            AutoScaleMode = AutoScaleMode.None;
+            BackColor = SystemColors.ActiveCaption;
+            CancelButton = button1;
+            ClientSize = new Size(1487, 600);
+            Controls.Add(button1);
+            Controls.Add(groupBox1);
+            Controls.Add(dataGridView1);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "Конвертер статистики";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private OpenFileDialog openFileDialog1;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
+        private DataGridViewTextBoxColumn Column5;
+        private DataGridViewTextBoxColumn Column6;
+        private DataGridViewTextBoxColumn Column7;
+        private DataGridViewTextBoxColumn Column8;
+        private DataGridViewTextBoxColumn Column9;
+        private TextBox textBox1;
+        private GroupBox groupBox1;
+        internal Button button1;
+    }
+}
---
+++ OpenStatistic/Form1.cs
@@ -0,0 +1,193 @@
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using Microsoft.VisualBasic;
+using Excel = Microsoft.Office.Interop.Excel;
+using System;
+using System.Windows.Forms;
+using DocumentFormat.OpenXml.Bibliography;
+using DocumentFormat.OpenXml.InkML;
+using System.Diagnostics;
+using Microsoft.Office.Interop.Excel;
+using System.Text;
+using DocumentFormat.OpenXml.Office2016.Excel;
+using System.Drawing;
+
+namespace OpenStatistic
+
+{
+    public partial class Form1 : Form
+    {
+        public bool m_Saving = false;
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+
+        }
+        public void OpenxlsFile(string tbPath)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        public void CloseProcess()
+        {
+            Process[] List;
+            List = Process.GetProcessesByName("EXCEL");
+            foreach (Process proc in List)
+            {
+                proc.Kill();
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+
+        }
+
+        private void KillSpecificExcelFileProcess(string excelFileName)
+        {
+            var processes = from p in Process.GetProcessesByName("EXCEL")
+                            select p;
+            foreach (var process in processes)
+            {
+                if (process.MainWindowTitle == "Microsoft Excel - " + excelFileName)
+                    process.Kill();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Excel.Application xApp;
+            Microsoft.Office.Interop.Excel.Workbook workbook;
+
+            Microsoft.Office.Interop.Excel.Worksheet worksheet;
+            Microsoft.Office.Interop.Excel.Range range;
+            string fullname = "";
+            CloseProcess();
+
+            int xlRow;
+            string filename;
+            openFileDialog1.Filter = "Excel Office | *CSV";
+           // openFileDialog1.ShowDialog();
+            
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filename = openFileDialog1.FileName;
+                if (filename != string.Empty)
+                {
+                    textBox1.Text = filename;
+                    xApp = new Microsoft.Office.Interop.Excel.Application();
+                    workbook = xApp.Workbooks.Open(filename);
+                    string pathfile = Path.GetDirectoryName(filename);
+                    string fName = Path.GetFileNameWithoutExtension(filename);
+                    worksheet = workbook.Worksheets[1];
+                    range = worksheet.UsedRange;
+                    dataGridView1.Rows.Clear();
+                    dataGridView1.Refresh();
+                    for (xlRow = 2; xlRow <= range.Rows.Count ; xlRow++)
+                    {
+                        
+                        dataGridView1.Rows.Add((range.Cells[xlRow, 1].Text).Split(";"));
+                    }
+
+                    worksheet = workbook.Worksheets.Add(workbook.Worksheets[1]);
+
+                    worksheet.Activate();
+                   // worksheet.Name = "Result";
+                    worksheet.Columns.EntireColumn.AutoFit();
+                    worksheet.Range["A1"].Value = "Номер программы";
+                    worksheet.Range["B1"].Value = "Дата";
+                    worksheet.Range["C1"].Value = "Программа";
+                    worksheet.Range["D1"].Value = "Время цикла";
+                    worksheet.Range["E1"].Value = "Время устранения остановки";
+                    worksheet.Range["F1"].Value = "Время работы робота 1";
+                    worksheet.Range["G1"].Value = "Время работы робота 2";
+                    worksheet.Range["H1"].Value = "Количество";
+                    worksheet.Range["I1"].Value = "Логин";
+
+                    for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                    {
+                        for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                        {
+                            xApp.Cells[i + 2, j + 1] = dataGridView1.Rows[i].Cells[j].Value.ToString().Trim(new Char[] { '"'});
+                        }
+                    }
+                    //fName = fName.Substring(0, 15);
+                    workbook.SaveAs(pathfile + "/Convert_" + fName, Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault);
+                    
+                    //using (SaveFileDialog sfd = new SaveFileDialog() { Filter = "Excel Workbook|*.xlsx", ValidateNames = true })
+                    //{
+                        
+                    //    if (sfd.ShowDialog() == DialogResult.OK)
+                    //    {
+                    //        fullname = sfd.FileName;
+                    //        workbook.SaveAs(sfd.FileName, Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault);
+                    //    }
+                    //    else
+                    //    {
+                            
+                    //    }
+
+                    //}
+
+                    //workbook.Save();
+                    // workbook.SaveAs(filename, Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault);
+                    workbook.Close(); 
+                    xApp.Quit();
+
+
+
+                    KillSpecificExcelFileProcess("filename");
+                   
+                    MessageBox.Show("Файл сохранен",fullname, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            { }
+        }
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            button1.Enabled = true;
+            button1.UseVisualStyleBackColor = false;
+            button1.BackColor = System.Drawing.Color.Green;
+        }
+
+        private void button1_MouseUp(object sender, MouseEventArgs e)
+        {
+           
+        }
+    }
+}
+
+
+
